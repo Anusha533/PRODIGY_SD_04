e682898f5f18d65b9a29e8972034c202aafabc22
Anusha533 <anu343926@gmail.com>
2024-08-05 18:48:49 +0530

    Add files via upload

---
+++ SudokuSolver.java
@@ -0,0 +1,85 @@
+public class SudokuSolver {
+
+    private static final int GRID_SIZE = 9;
+
+    public static void main(String[] args) {
+        int[][] board = {
+            {5, 3, 0, 0, 7, 0, 0, 0, 0},
+            {6, 0, 0, 1, 9, 5, 0, 0, 0},
+            {0, 9, 8, 0, 0, 0, 0, 6, 0},
+            {8, 0, 0, 0, 6, 0, 0, 0, 3},
+            {4, 0, 0, 8, 0, 3, 0, 0, 1},
+            {7, 0, 0, 0, 2, 0, 0, 0, 6},
+            {0, 6, 0, 0, 0, 0, 2, 8, 0},
+            {0, 0, 0, 4, 1, 9, 0, 0, 5},
+            {0, 0, 0, 0, 8, 0, 0, 7, 9}
+        };
+
+        if (solveBoard(board)) {
+            System.out.println("Sudoku puzzle solved:");
+            printBoard(board);
+        } else {
+            System.out.println("Unsolvable puzzle.");
+        }
+    }
+
+    private static boolean solveBoard(int[][] board) {
+        for (int row = 0; row < GRID_SIZE; row++) {
+            for (int col = 0; col < GRID_SIZE; col++) {
+                if (board[row][col] == 0) {
+                    for (int num = 1; num <= GRID_SIZE; num++) {
+                        if (isValid(board, num, row, col)) {
+                            board[row][col] = num;
+
+                            if (solveBoard(board)) {
+                                return true;
+                            } else {
+                                board[row][col] = 0;
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static boolean isValid(int[][] board, int num, int row, int col) {
+        // Check if the number is not repeated in the current row, column, and 3x3 sub-grid
+        for (int i = 0; i < GRID_SIZE; i++) {
+            if (board[row][i] == num || board[i][col] == num) {
+                return false;
+            }
+        }
+
+        int boxRowStart = row - row % 3;
+        int boxColStart = col - col % 3;
+
+        for (int i = boxRowStart; i < boxRowStart + 3; i++) {
+            for (int j = boxColStart; j < boxColStart + 3; j++) {
+                if (board[i][j] == num) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private static void printBoard(int[][] board) {
+        for (int row = 0; row < GRID_SIZE; row++) {
+            if (row % 3 == 0 && row != 0) {
+                System.out.println("-----------");
+            }
+            for (int col = 0; col < GRID_SIZE; col++) {
+                if (col % 3 == 0 && col != 0) {
+                    System.out.print("|");
+                }
+                System.out.print(board[row][col] == 0 ? "." : board[row][col]);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
